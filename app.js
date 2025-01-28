1f2b0d4115fcbaf3998a7925c1d4db8bf218bc8d
Jayler Castillo <jayler0203@gmail.com>
2025-01-28 09:42:32 -0500

    Agregar javascript

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo = document.getElementById("amigo");
+let amigos = [];
+
+function agregarAmigo() {
+  if (inputAmigo.value.trim() == "") {
+    alert("Por favor, inserte un nombre.");
+    return;
+  }
+  amigos.push(inputAmigo.value.trim());
+  inputAmigo.value = "";
+  actualizarListaAmigos();
+}
+function actualizarListaAmigos() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    lista.innerHTML += `
+        <li>${amigos[i]}<li>`;
+  }
+}
+
+function sortearAmigo() {
+  if (amigos.length > 0) {
+    indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    mostrarResultado(indiceAleatorio);
+    amigos.splice(indiceAleatorio, 1);
+    actualizarListaAmigos();
+  } else {
+    alert("Por favor, inserte un nombre.");
+  }
+}
+function mostrarResultado(indice) {
+  const resultado = document.getElementById("resultado");
+  const amigoSeleccionado = amigos[indice];
+  resultado.innerHTML = amigoSeleccionado;
+}
